46ec1eb3328746c69dbeda8baedd6d338d19d048
Aidan Perret <aperret@me.com>
2021-07-20 18:28:49 -0400

    commit tree 9584f0675d1aa7561ca91c1953afaeb9f182a93e

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,21 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+  <nav>
+    {links.map((link) => {
+    return <a key={link} href={`#${link}`}>{link}</a>
+    })}
+  </nav>)
 }
-
 export default NavBar;
+
+//ALT METHOD
+// function NavBar() {
+//   const links = ["home", "about", "projects"];
+//   const navigation = links.map((link) => {
+//     return <a key={link} href={`#${link}`}>{link}</a>
+//   })
+//   return <nav>{navigation}</nav>;
+// }
+// export default NavBar;
